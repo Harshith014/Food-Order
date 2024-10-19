ae10e279a8d63401e4e497e359214b510ef81cb9
harshith <harshith2742@gmail.com>
2024-10-20 00:35:22 +0530

    menu load bar inserted

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -15,6 +15,7 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import tableImage from "../assets/table.jpg";
 import DescriptionAlerts from "../components/Alert";
+import LoadingComponent from "../components/Loading";
 import { AuthContext } from "../context/UserContext";
 
 const MenuPage = () => {
@@ -23,23 +24,28 @@ const MenuPage = () => {
   const [alertStatus, setAlertStatus] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [pageSize] = useState(10); // display 10 items per page
+  const [isLoading, setIsLoading] = useState(true);
   const [totalItems, setTotalItems] = useState(0); // Store total item count
   const theme = useTheme();
 
-  
   useEffect(() => {
     const fetchMenuItems = async () => {
+      setIsLoading(true); // Start loading
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_REACT_APP_URI}/api/menu/allMenu?page=${pageNumber}&pageSize=${pageSize}`
+          `${
+            import.meta.env.VITE_REACT_APP_URI
+          }/api/menu/allMenu?page=${pageNumber}&pageSize=${pageSize}`
         );
-  
+
         // Extract the data and set state
         setMenuItems(response.data.menuItems);
         setTotalItems(response.data.totalItems); // Set the total number of items
       } catch (error) {
         console.error("Error fetching menu items:", error);
         setAlertStatus("error");
+      } finally {
+        setIsLoading(false); // Stop loading
       }
     };
     fetchMenuItems();
@@ -76,7 +82,7 @@ const MenuPage = () => {
     } finally {
       setTimeout(() => {
         setAlertStatus(null);
-      }, 3000); 
+      }, 3000);
     }
   };
 
@@ -114,123 +120,127 @@ const MenuPage = () => {
             }}
           />
         )}
-        <Grid container spacing={4}>
-          {menuItems.map((item, index) => (
-            <Grid
-              item
-              key={`${item._id}-${index}`}
-              xs={12}
-              sm={6}
-              md={4}
-              lg={3}
-              sx={{
-                [theme.breakpoints.down("sm")]: {
-                  maxWidth: "100%",
-                },
-              }}
-            >
-              <Card
+        {isLoading ? ( // Conditionally render LoadingComponent
+          <LoadingComponent /> // Show loading indicator while fetching data
+        ) : (
+          <Grid container spacing={4}>
+            {menuItems.map((item, index) => (
+              <Grid
+                item
+                key={`${item._id}-${index}`}
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
                 sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  borderRadius: 16,
-                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-                  backgroundColor: "#fff5e6",
+                  [theme.breakpoints.down("sm")]: {
+                    maxWidth: "100%",
+                  },
                 }}
               >
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={item.image}
-                  alt={item.name}
-                  loading="lazy" // Lazy load the images
+                <Card
                   sx={{
-                    objectFit: "cover",
-                    borderRadius: "16px 16px 0 0",
-                    [theme.breakpoints.down("sm")]: {
-                      height: 150,
-                    },
-                  }}
-                />
-                <CardContent
-                  sx={{
-                    flexGrow: 1,
+                    height: "100%",
                     display: "flex",
                     flexDirection: "column",
-                    padding: 2,
-                    backgroundColor: "#f9f3e6",
+                    borderRadius: 16,
+                    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                    backgroundColor: "#fff5e6",
                   }}
                 >
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={item.image}
+                    alt={item.name}
+                    loading="lazy" // Lazy load the images
                     sx={{
-                      fontSize: 18,
-                      fontWeight: 500,
-                      color: "#964B00",
+                      objectFit: "cover",
+                      borderRadius: "16px 16px 0 0",
                       [theme.breakpoints.down("sm")]: {
+                        height: 150,
+                      },
+                    }}
+                  />
+                  <CardContent
+                    sx={{
+                      flexGrow: 1,
+                      display: "flex",
+                      flexDirection: "column",
+                      padding: 2,
+                      backgroundColor: "#f9f3e6",
+                    }}
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      sx={{
+                        fontSize: 18,
+                        fontWeight: 500,
+                        color: "#964B00",
+                        [theme.breakpoints.down("sm")]: {
+                          fontSize: 16,
+                        },
+                      }}
+                    >
+                      {item.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{
+                        fontSize: 14,
+                        lineHeight: 1.5,
+                        [theme.breakpoints.down("sm")]: {
+                          fontSize: 12,
+                        },
+                      }}
+                    >
+                      {item.description}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      color="primary"
+                      sx={{
                         fontSize: 16,
-                      },
-                    }}
-                  >
-                    {item.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{
-                      fontSize: 14,
-                      lineHeight: 1.5,
-                      [theme.breakpoints.down("sm")]: {
-                        fontSize: 12,
-                      },
-                    }}
-                  >
-                    {item.description}
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    color="primary"
-                    sx={{
-                      fontSize: 16,
-                      fontWeight: 500,
-                      mt: 2,
-                      color: "#FFC107",
-                      [theme.breakpoints.down("sm")]: {
-                        fontSize: 14,
-                      },
-                    }}
-                  >
-                    ${item.price}
-                  </Typography>
-                  <Button
-                    variant="contained"
-                    color="warning"
-                    startIcon={<AddShoppingCart />}
-                    sx={{
-                      mt: "auto",
-                      borderRadius: 16,
-                      padding: "12px 24px",
-                      backgroundColor: "#FFC107",
-                      "&:hover": {
-                        backgroundColor: "#ff9800",
-                      },
-                      [theme.breakpoints.down("sm")]: {
-                        padding: "10px 20px",
-                        fontSize: 14,
-                      },
-                    }}
-                    onClick={() => handleAddToCart(item)}
-                  >
-                    Add to Cart
-                  </Button>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+                        fontWeight: 500,
+                        mt: 2,
+                        color: "#FFC107",
+                        [theme.breakpoints.down("sm")]: {
+                          fontSize: 14,
+                        },
+                      }}
+                    >
+                      ${item.price}
+                    </Typography>
+                    <Button
+                      variant="contained"
+                      color="warning"
+                      startIcon={<AddShoppingCart />}
+                      sx={{
+                        mt: "auto",
+                        borderRadius: 16,
+                        padding: "12px 24px",
+                        backgroundColor: "#FFC107",
+                        "&:hover": {
+                          backgroundColor: "#ff9800",
+                        },
+                        [theme.breakpoints.down("sm")]: {
+                          padding: "10px 20px",
+                          fontSize: 14,
+                        },
+                      }}
+                      onClick={() => handleAddToCart(item)}
+                    >
+                      Add to Cart
+                    </Button>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
         <Pagination
           count={Math.ceil(totalItems / pageSize)} // Calculate total pages from totalItems
           page={pageNumber}
@@ -239,12 +249,12 @@ const MenuPage = () => {
             mt: 2,
             mb: 2,
             display: "flex",
-            justifyContent: "center", 
+            justifyContent: "center",
             "& .MuiPaginationItem-root": {
               fontSize: 30,
               fontWeight: 500,
               color: "#ffd0a2",
-              borderRadius: 16, 
+              borderRadius: 16,
               "&:hover": {
                 backgroundColor: "#ff9800",
               },
@@ -252,7 +262,7 @@ const MenuPage = () => {
             "& .MuiPaginationItem-root.Mui-selected": {
               backgroundColor: "#FFC107",
               color: "#fff",
-              borderRadius: 16, 
+              borderRadius: 16,
             },
           }}
         />
